bdf20e13596bb943d0b50514d5d06b25eb4cff11
Gerry Agbobada <gagbobada+git@gmail.com>
2018-05-01 15:39:09 +0200

    Tweak values to have a fast-but-not-too-fast Entity

--- src/entity/entity.h
+++ src/entity/entity.h
@@ -154,7 +154,7 @@ class Entity : public QGraphicsItem {
     QVector2D _acc{0, 0};                //!< Acceleration
     float _vel_angle{0};                 //!< Current angle of the velocity
     float _mass{1};                      //!< Mass of the Entity
-    float _max_force{1};                 //!< Maximum force the
+    float _max_force{3};                 //!< Maximum force the
                                          //!< Entity can apply to move itself
     float _linear_vel_friction_coef{10};  //!< friction coefficient
                                          //!< for linear velocity
